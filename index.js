ccf71bed42c8bed4bd1df65400dae99aad6cb9a1
Archit Bhonsle <abhonsle2000@gmail.com>
2020-10-11 16:02:12 +0530

    changes to host on godaddy

--- index.js
+++ index.js
@@ -5,33 +5,33 @@ const prepareEmailFields = require('./prepareEmailFields');
 require('dotenv').config();
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 const transporter = nodeMailer.createTransport({
-    service : 'gmail',
-    auth    : {
-        user : process.env.SENDER_MAIL,
-        pass : process.env.SENDER_PASS
-    }
+  service : 'gmail',
+  auth    : {
+    user : process.env.SENDER_MAIL,
+    pass : process.env.SENDER_PASS
+  }
 });
 
 app.post('/', (req, res) => {
-    try {
-        const mailOptions = prepareEmailFields(req.body);
-        transporter.sendMail(mailOptions, (err, info) => {
-            console.log({ err, info });
-        });
-        res.sendStatus(200);
-    } catch (err) {
-        console.log(err);
-        res.send(500);
-    }
+  try {
+    const mailOptions = prepareEmailFields(req.body);
+    transporter.sendMail(mailOptions, (err, info) => {
+      console.log({ err, info });
+    });
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.send(500);
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Live at http://localhost:${PORT}`);
+  console.log(`Live at http://localhost:${PORT}`);
 });
